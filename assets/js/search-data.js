6327f41087b3be52832bec6c6345181bca28dcf2
levioconnor <oconnorlevi712@gmail.com>
2026-05-19 21:04:08 +0000

    Deploying to gh-pages from @ levioconnor/levioconnor.github.io@02bf34122254424d1c85a992b682e39996c7939c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,15 +391,15 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-ku-announces-beinecke-scholarship-nominee",
+            },},{id: "news-students-to-present-at-undergraduate-research-colloquium",
+          title: 'Students to present at Undergraduate Research Colloquium',
+          description: "",
+          section: "News",},{id: "news-ku-announces-beinecke-scholarship-nominee",
           title: 'KU announces Beinecke scholarship nominee',
           description: "",
           section: "News",},{id: "projects-project-1",
