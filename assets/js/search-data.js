d267660f15e594e84bf5c4834f3ae7f6c0a3f004
levioconnor <oconnorlevi712@gmail.com>
2026-05-20 19:45:46 +0000

    Deploying to gh-pages from @ levioconnor/levioconnor.github.io@a6659e95dc22a9b6532f0c7ba7ef28f504f2412b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-news",
-          title: "News",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/News/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
