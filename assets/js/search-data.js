4487682f40fdb6eaf8a6a5e2cb44153f7c7f1bbe
levioconnor <oconnorlevi712@gmail.com>
2026-06-15 23:56:23 +0000

    Deploying to gh-pages from @ levioconnor/levioconnor.github.io@ac0c431d033d0122ea8132026f633b9bd3e09229 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of my academic and personal projects.",
+          description: "A growing collection of my academic projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
