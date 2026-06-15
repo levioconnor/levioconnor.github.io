7feb6c2411a75f3c33a3a4b5d004251e6cdb4ada
levioconnor <oconnorlevi712@gmail.com>
2026-06-15 23:26:46 +0000

    Deploying to gh-pages from @ levioconnor/levioconnor.github.io@3ba0d1a44ba8ebe893a0bdc1224ba4b7036e6644 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,9 +408,9 @@ ninja.data = [{
           description: "Society for Personality and Social Psychology, 2026",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-kckcc-2026-poster",
+          title: 'KCKCC 2026 Poster',
+          description: "Kansas City Kansas Community College Research Conference in the Psychological Sciences, 2026",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
